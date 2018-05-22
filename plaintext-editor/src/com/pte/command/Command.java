74d8e4e32ddd99aca826f710d87a37f62d4af7d5
Stephen Czekalski <admin@stephencz.com>
2018-05-21 23:14:07 -0400

    Added 'relaxed' theme.

--- plaintext-editor/src/com/pte/command/Command.java
+++ plaintext-editor/src/com/pte/command/Command.java
@@ -7,15 +7,25 @@ import com.pte.editor.Editor;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The Command class represents a command that performs some function.
+ * Every command has an identifier which acts as its trigger. For example,
+ * a command which opens a file might have an identifier of 'open'.
+ * @author stephen
+ *
+ */
 public abstract class Command
 {
 
+	/**A reference to the editor that the command can manipulate.*/
 	protected final Editor editor;
 	
+	/**A reference to the command line where the command was passed.*/
 	protected final CommandLine commandLine;
 	
+	/**The command's identifier.*/
 	protected final String identifier;
-	
+
 	public Command(Editor editor, CommandLine commandLine, String identifier)
 	{
 		this.editor = editor;
